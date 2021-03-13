303b02d3631939e7e1f762d47640597b04b75406
Grigoriy <grisha101202@yandex,ru>
2021-03-13 22:39:34 +0300

    ended app

--- app/src/main/java/com/example/randomnumberfrom1to100/MainActivity.java
+++ app/src/main/java/com/example/randomnumberfrom1to100/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.randomnumberfrom1to100;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView2);
+    }
+
+    public void generated(View view) {
+        int random = (int) (Math.random()*100);
+        textView.setText(Integer.toString(random));
     }
 }
